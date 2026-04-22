829893fa370d96494c1ed7e56ef344838ab9b27c
joh <johanne.bohn@ensae.fr>
2026-04-22 18:14:43 +0000

    passage au gpu

--- data_restructuration.py
+++ data_restructuration.py
@@ -1,168 +1,230 @@
-import torch
-from torchvision.models import ResNet18_Weights
-import torchvision.models as models
+import os
+import shutil
+import cv2 
 import numpy as np
-from torchvision.transforms import transforms
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
-from torch.utils.data import random_split   
-import matplotlib.pyplot as plt
-import torch.nn.functional as F
-import torch.optim as optim
+from pathlib import Path
 from tqdm import tqdm
 import seaborn as sns
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.model_selection import StratifiedShuffleSplit
-from torch.utils.data import Subset
-from torch.utils.data import WeightedRandomSampler
-from collections import Counter
 
 
 def restructure_normale_data(
-    source,
+    data_local,
     destination
 ):
+    source = data_local+"Normal/"
+    destination = destination+"0-normal/"
+    os.makedirs(destination, exist_ok=True)
+    
     for filename in os.listdir(source):
-        source_filename= source + "\\" + filename
-        chemin_MLO_right=os.path.join(source_filename,"A_"+filename+"_1.RIGHT_MLO.jpg")
-        chemin_MLO_left=os.path.join(source_filename,"A_"+filename+"_1.LEFT_MLO.jpg")
+        source_filename = os.path.join(source, filename)
+        if not os.path.isdir(source_filename):
+            continue
+            
+        chemin_MLO_right = os.path.join(source_filename, "A_" + filename + "_1.RIGHT_MLO.jpg")
+        chemin_MLO_left = os.path.join(source_filename, "A_" + filename + "_1.LEFT_MLO.jpg")
+        
         if os.path.exists(chemin_MLO_right):
-            shutil.copy2(chemin_MLO_right,destination)
+            shutil.copy2(chemin_MLO_right, destination)
         if os.path.exists(chemin_MLO_left):
-            shutil.copy2(chemin_MLO_left,destination)
-    print("Restructuration terminée")
+            shutil.copy2(chemin_MLO_left, destination)
+    
+    print("✓ Restructuration Normal terminée")
 
 
 def restructuration_cancer(
-    source,
-    destination1,
-    destination2
+    data_local,
+    destination
 ):
-    c=0
+    source = data_local+"Cancer/"
+    destination1 = destination+"1-cancer_benign/"
+    destination2 = destination+"0-normal/"
+    os.makedirs(destination1, exist_ok=True)
+    os.makedirs(destination2, exist_ok=True)
+    
     for filename in os.listdir(source):
-        #identification de la lettre correcte
-        ok=False
-        i=0
-        l=["A_","B_","C_","D_"]
-        source_filename= source + "\\" + filename
-        while ok == False:
-            chemin_MLO_right=os.path.join(source_filename,l[i]+filename+"_1.RIGHT_MLO.jpg")
+        source_filename = os.path.join(source, filename)
+        if not os.path.isdir(source_filename):
+            continue
+        
+        # Identification de la lettre correcte
+        ok = False
+        i = 0
+        l = ["A_", "B_", "C_", "D_"]
+        
+        while not ok and i < 4:
+            chemin_MLO_right = os.path.join(source_filename, l[i] + filename + "_1.RIGHT_MLO.jpg")
             if os.path.exists(chemin_MLO_right):
                 ok = True
             else:
-                i+=1
-        if i==4:
-            print("erreur1 avec le fichier "+filename)
-
-        #identification cancer gauche ou droit
-        orientation=None
-        if os.path.exists(os.path.join(source_filename,l[i]+filename+"_1.RIGHT_MLO.OVERLAY")):
-             orientation="RIGHT"
-        elif os.path.exists(os.path.join(source_filename,l[i]+filename+"_1.LEFT_MLO.OVERLAY")):
-            orientation="LEFT"
+                i += 1
+        
+        if i == 4:
+            print(f"Erreur1 avec le fichier {filename}")
+            continue
+        
+        # Identification cancer gauche ou droit
+        orientation = None
+        if os.path.exists(os.path.join(source_filename, l[i] + filename + "_1.RIGHT_MLO.OVERLAY")):
+            orientation = "RIGHT"
+        elif os.path.exists(os.path.join(source_filename, l[i] + filename + "_1.LEFT_MLO.OVERLAY")):
+            orientation = "LEFT"
         else:
-             print("cancer uniquement visible sur le point de vue CC "+filename)
-        #copie des images
-        if orientation=="RIGHT":
-            shutil.copy2(chemin_MLO_right,destination1)
-            shutil.copy2(os.path.join(source_filename,l[i]+filename+"_1.LEFT_MLO.jpg"),destination2)
-        if orientation=="LEFT":
-            shutil.copy2(os.path.join(source_filename,l[i]+filename+"_1.LEFT_MLO.jpg"),destination1)
-            shutil.copy2(chemin_MLO_right,destination2)
+            print(f"Cancer uniquement visible sur le point de vue CC : {filename}")
+        
+        # Copie des images
+        if orientation == "RIGHT":
+            shutil.copy2(chemin_MLO_right, destination1)
+            chemin_MLO_left = os.path.join(source_filename, l[i] + filename + "_1.LEFT_MLO.jpg")
+            if os.path.exists(chemin_MLO_left):
+                shutil.copy2(chemin_MLO_left, destination2)
+        elif orientation == "LEFT":
+            chemin_MLO_left = os.path.join(source_filename, l[i] + filename + "_1.LEFT_MLO.jpg")
+            if os.path.exists(chemin_MLO_left):
+                shutil.copy2(chemin_MLO_left, destination1)
+            if os.path.exists(chemin_MLO_right):
+                shutil.copy2(chemin_MLO_right, destination2)
+    
+    print("Restructuration Cancer terminée")
 
 
 def restructuration_benign(
-    source,
-    destination1,
-    destination2
+    data_local,
+    destination
 ):
+    source = data_local+"Benign/"
+    destination1 = destination+"1-cancer_benign/"
+    destination2 = destination+"0-normal/"
+    os.makedirs(destination1, exist_ok=True)
+    os.makedirs(destination2, exist_ok=True)
+    
     for filename in os.listdir(source):
-        #identification de la lettre correcte
-        ok=False
-        i=0
-        l=["A_","B_","C_","D_"]
-        source_filename= source + "\\" + filename
-        while ok == False:
-            chemin_MLO_right=os.path.join(source_filename,l[i]+filename+"_1.RIGHT_MLO.jpg")
+        source_filename = os.path.join(source, filename)
+        if not os.path.isdir(source_filename):
+            continue
+        
+        # Identification de la lettre correcte
+        ok = False
+        i = 0
+        l = ["A_", "B_", "C_", "D_"]
+        
+        while not ok and i < 4:
+            chemin_MLO_right = os.path.join(source_filename, l[i] + filename + "_1.RIGHT_MLO.jpg")
             if os.path.exists(chemin_MLO_right):
                 ok = True
             else:
-                i+=1
-        if i==4:
-            print("erreur1 avec le fichier "+filename)
-
-        #identification cancer gauche ou droit
-        orientation=None
-        if os.path.exists(os.path.join(source_filename,l[i]+filename+"_1.RIGHT_MLO.OVERLAY")):
-             orientation="RIGHT"
-        elif os.path.exists(os.path.join(source_filename,l[i]+filename+"_1.LEFT_MLO.OVERLAY")):
-            orientation="LEFT"
+                i += 1
+        
+        if i == 4:
+            print(f"Erreur1 avec le fichier {filename}")
+            continue
+        
+        # Identification benign gauche ou droit
+        orientation = None
+        if os.path.exists(os.path.join(source_filename, l[i] + filename + "_1.RIGHT_MLO.OVERLAY")):
+            orientation = "RIGHT"
+        elif os.path.exists(os.path.join(source_filename, l[i] + filename + "_1.LEFT_MLO.OVERLAY")):
+            orientation = "LEFT"
         else:
-             print("cancer uniquement visible sur le point de vue CC "+filename)
-        #copie des images
-        if orientation=="RIGHT":
-            shutil.copy2(chemin_MLO_right,destination1)
-            shutil.copy2(os.path.join(source_filename,l[i]+filename+"_1.LEFT_MLO.jpg"),destination2)
-        if orientation=="LEFT":
-            shutil.copy2(os.path.join(source_filename,l[i]+filename+"_1.LEFT_MLO.jpg"),destination1)
-            shutil.copy2(chemin_MLO_right,destination2)
+            print(f"Benign uniquement visible sur le point de vue CC : {filename}")
+        
+        # Copie des images
+        if orientation == "RIGHT":
+            shutil.copy2(chemin_MLO_right, destination1)
+            chemin_MLO_left = os.path.join(source_filename, l[i] + filename + "_1.LEFT_MLO.jpg")
+            if os.path.exists(chemin_MLO_left):
+                shutil.copy2(chemin_MLO_left, destination2)
+        elif orientation == "LEFT":
+            chemin_MLO_left = os.path.join(source_filename, l[i] + filename + "_1.LEFT_MLO.jpg")
+            if os.path.exists(chemin_MLO_left):
+                shutil.copy2(chemin_MLO_left, destination1)
+            if os.path.exists(chemin_MLO_right):
+                shutil.copy2(chemin_MLO_right, destination2)
+    
+    print("Restructuration Benign terminée")
 
 
-def get_transformed_data(
-    image_length, 
-    image_heigth, 
-    root
+def process_mammo(
+    image_path,
+    rh,
+    rb,
+    valeur_de_coupure,
+    rhp
 ):
-    transform = transforms.Compose([
-        transforms.Resize((image_length,image_heigth)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-    data_set=ImageFolder(root=root, transform=transform)
-    return dataset
+    img=cv2.imread(image_path,0) #on récupère l'image
+    hauteur,largeur=img.shape
+    img=img[rh:hauteur-rb,:] #on rogne le haut et le bas de 20 pixels ccar ils sont trop clairs et ils modifient le masque
+    _,thresh=cv2.threshold(img,valeur_de_coupure,255,cv2.THRESH_BINARY) #on isole les formes
+    contours,_=cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) #on récupère les contours
+    large_cnt=max(contours,key=cv2.contourArea) #on récupère le plus grand contours correspondant au sein
+    mask=np.zeros_like(img)
+    cv2.drawContours(mask,[large_cnt],-1,255,-1) #on créée une image noir avec le contour du sein rempli de blanc 
+    clean_img=cv2.bitwise_and(img,mask) # -> ce qui est rempli de blanc reste, ce qui est rempli de noir devient noir
+    x, y, w, h = cv2.boundingRect(large_cnt) # on récupère les valeurs extrèmes du contour
+    cropped_img = clean_img[y:y+h, x:x+w] # pour rogner 
+    new_hauteur, new_largeur=cropped_img.shape #maintenant on met aux proportion du vindr en 1520*912 
+    (vindr_hauteur, vindr_largeur)=(1520,912)  #pour ne pas avoir de déformation différente des images de celles du vindr
+    rapport_vindr=vindr_hauteur/vindr_largeur   #pour ce faire, comme la largeur est fixée de la même manière dans les deux datasets (début et bout du sein en largeur)
+    new_hauteur_goal=rapport_vindr*new_largeur  #on a juste à ajuster la hauteur
+    difference_pixel=int(new_hauteur-new_hauteur_goal) #sinon on ajuste pas et on voit s'il n'y a pas trop d'images sur lesquelles on ne réajuste pas les proportions
+    proportion_pas_ok=1
+    if difference_pixel>0:
+        rognage_en_haut=int(rhp*difference_pixel)
+        rognage_en_bas=difference_pixel-rognage_en_haut
+        cropped_img=cropped_img[rognage_en_haut:new_hauteur-rognage_en_bas,:]
+        proportion_pas_ok=0 #pour connaître le nombre d'image pas aux proportions de celles du vindr
+    return((cropped_img,proportion_pas_ok))    
 
 
-def get_data_loaders(
-    dataset,
-    numero_random,
-    batch_size,
-    train_size,
-    val_size,
-    test_size
+
+def traitement_image(
+    chemin_dossier_destination,
+    source_data_normal,
+    source_data_cancer_benign,
+    rh,
+    rb,
+    valeur_de_coupure,
+    rhp,
+    res_min
 ):
-    labels = np.array(dataset.targets)
-    sss1=StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=numero_random)
-    for train_val_index, test_index in sss1.split(np.zeros(len(labels)), labels):
-        train_val_indices=train_val_index
-        test_indices=test_index
-    train_val_labels=labels[train_val_indices]
-    new_val_size=val_size/(train_size+val_size)
-    ss2=StratifiedShuffleSplit(n_splits=1, test_size=new_val_size, random_state=numero_random)
-    for train_index, val_index in ss2.split(np.zeros(len(train_val_labels)), train_val_labels):
-        train_indices=train_val_indices[train_index]
-        val_indices=train_val_indices[val_index]
-    train_labels=labels[train_indices]
-    class_counts=Counter(train_labels)
-    print(f"Comptes des classes dans le jeu d'entraînement: {class_counts} \n")
+    path_obj_normal = Path(source_data_normal)
+    path_obj_cancer_benign= Path(source_data_cancer_benign)
+    image_normal_paths = list(path_obj_normal.glob("*.jpg")) 
+    image_cancer_paths = list(path_obj_cancer_benign.glob("*.jpg"))
     
-    num_labels=len(train_labels)
-    weight_for_class={cl : num_labels/count for cl, count in class_counts.items()}
-    sample_weights=[weight_for_class[l] for l in train_labels]
-    sample_weights_tensor=torch.DoubleTensor(sample_weights)
-    print(f"poids calculés par classe :{weight_for_class} \n")
-    
-    sampler = WeightedRandomSampler(weights=sample_weights_tensor, num_samples=len(sample_weights_tensor), replacement=True)
-    distribution_sampler=Counter(train_labels[list(sampler)])
-    print(f"Distribution des classes dans le sampler : {distribution_sampler} \n")
-    
-    train_set=Subset(data_set, train_indices)
-    val_set=Subset(data_set, val_indices)
-    test_set=Subset(data_set, test_indices)
-    print(f"Train set size: {len(train_set)}")
-    print(f"Validation set size: {len(val_set)}")
-    print(f"Test set size: {len(test_set)}")
-    
-    train_loader = DataLoader(train_set, batch_size=batch_size, sampler=sampler, shuffle=False)
-    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
-    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
-    
-    return train_loader, val_loader, test_loader
+    destination_cancer_benign= os.path.join(chemin_dossier_destination,"1-cancer_benign")
+    destination_normal=os.path.join(chemin_dossier_destination,"0-normal")
+    os.makedirs(chemin_dossier_destination, exist_ok=True)
+    os.makedirs(destination_cancer_benign, exist_ok=True)
+    os.makedirs(destination_normal, exist_ok=True)
+    compteur_proportion_pas_ok=0
+    i=0
+
+    for image in tqdm(image_normal_paths):
+        (new_image,b)=process_mammo(
+            image,
+            rh,
+            rb,
+            valeur_de_coupure,
+            rhp
+        )
+        (hauteur,largeur)=new_image.shape
+        if b==0 and hauteur>=res_min and largeur >=res_min:
+            cv2.imwrite(os.path.join(destination_normal, f"image_{i}.jpg"), new_image)
+        else:
+            compteur_proportion_pas_ok=compteur_proportion_pas_ok+b
+        i+=1
+    for image in tqdm(image_cancer_paths):
+        (new_image,b)=process_mammo(
+            image,
+            rh,
+            rb,
+            valeur_de_coupure,
+            rhp
+        )
+        (hauteur,largeur)=new_image.shape
+        if b==0 and hauteur>=res_min and largeur >=res_min:
+            cv2.imwrite(os.path.join(destination_cancer_benign, f"image_{i}.jpg"), new_image)
+        else:
+            compteur_proportion_pas_ok=compteur_proportion_pas_ok+b
+        i+=1
+    return(f"traitement_et_enregistrement_terminé au chemin {chemin_dossier_destination} \n proportion d'images non aux proportions vindr : {compteur_proportion_pas_ok/i} \n ATTENTION : penser à enregistrer sur onyxia")
--- training_model.py
+++ training_model.py
@@ -5,102 +5,196 @@ import numpy as np
 from torchvision.transforms import transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
-from torch.utils.data import random_split
+from torch.utils.data import random_split   
+import matplotlib.pyplot as plt
 import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm
+import seaborn as sns
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import StratifiedShuffleSplit
 from torch.utils.data import Subset
 from torch.utils.data import WeightedRandomSampler
 from collections import Counter
-from datetime import datetime
+import torch.nn as nn
+from torch.cuda.amp import GradScaler, autocast
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
+import gc
 
 
-def train_model(
-    model, 
-    train_loader, 
-    val_loader, 
-    criterion, 
-    optimizer
+def data_generation(
+    resolution,
+    batch_size,
+    contrast,
+    brightness,
+    degrees,
+    normalisation_choice,
+    lower_lim,
+    random_number,
+    train_size,
+    val_size,
+    test_size,
+    data_path
 ):
-    model.train()
-    running_loss = 0.0
-    for inputs, labels in tqdm(train_loader, desc="Training"):
-        inputs, labels = inputs.to(device), labels.to(device)
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss=criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item() * inputs.size(0)
-    epoch_loss= running_loss / len(train_loader.dataset)
-    return(epoch_loss)
+
+    #définition de la fonction de pour normaliser les images par rapport à elles mêmes
+    def robust_standardize1(x):
+        mask = x > lower_lim  #On crée un masque pour ignorer le fond (souvent proche de 0 ou < 0.05)
+        if mask.any():
+            mean = x[mask].mean()
+            std = x[mask].std()
+            x[mask] = (x[mask] - mean) / (std + 1e-6) 
+            x[mask] = torch.clamp(x[mask], -3, 3) #on ramène les valeurs extrêmes du sein à l'intervalle -3 3
+        x[~mask] = -4.0 # on fixe le reste des valeurs correspondantes au support noir à -4
+        return x
 
 
-def validate_model(
-    model, 
-    val_loader, 
-    criterion, 
-    device
-):
-    model.eval()
-    running_loss = 0.0
-    corrects = 0.0
-    with torch.no_grad():
-        for inputs, labels in tqdm(val_loader, desc="Validating"):
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            running_loss += loss.item() * inputs.size(0)
-            _,preds = torch.max(outputs, 1)
-            corrects += torch.sum(preds == labels.data)
-        epoch_loss = running_loss / len(val_loader.dataset)
-        epoch_acc = corrects.double() / len(val_loader.dataset)
-        return(epoch_loss, epoch_acc)
+    #definition de la fonction pour normaliser les images par rapport au dataset MiniDDSM et calcul de mean et std error du dataset
+    def calcul_dataset_mean_std():
+
+        def recuperation_pixel(loader_source, n_batches=20):
+            # On récupère quelques données pour avoir une distribution stable
+            source_pixels = []
+            
+            with torch.no_grad():
+                # Extraction du domaine Source
+                for i, (images, _) in tqdm(enumerate(loader_source)):
+                    if i >= n_batches: break
+                    # On prend un seul canal (gris) et on aplatit
+                    source_pixels.extend(images[:, 0, :, :].flatten().numpy())
+            return(source_pixels)
+
+        def robust_mean_variance(x):
+            # 1. On calcule la std error et la mean sur le dataset
+            mask = (x > 0.05) & (x<0.97) 
+            if mask.any():
+                mean = x[mask].mean()
+                std = x[mask].std()
+            return (mean,std)
+
+        train_transform = transforms.Compose([
+        transforms.Resize((resolution, resolution)),
+        transforms.ToTensor()])
+
+        train_dataset_full = ImageFolder(root=data_path, transform=train_transform)
+        train_loader1 = DataLoader(train_dataset_full, batch_size=batch_size)
+
+        (source_pixel)=recuperation_pixel(train_loader1)
+        source_pixel=np.array(source_pixel)
+
+        (mu,sigma)=robust_mean_variance(source_pixel)
+        return(mu,sigma)
+
+    def robust_standardize2(x):
+        mask = x > lower_lim
+        if mask.any():
+            mean = mu
+            std = sigma
+            x[mask] = (x[mask] - mean) / (std + 1e-6)    
+            x[mask] = torch.clamp(x[mask], -3, 3)
+        x[~mask] = -4.0 
+        return x
+
+    if normalisation_choice==0:
+        def identity(x):
+            return x
+        fonction = identity
+    elif normalisation_choice==1:
+        fonction=robust_standardize1
+    else:
+        (mu,sigma)=calcul_dataset_mean_std()
+        fonction=robust_standardize2
+        
+    # Transformation des données ATTENTION A L'ORDRE DES TRANSFORMS!!!
+    train_transform = transforms.Compose([
+        transforms.Resize((resolution, resolution)),
+        transforms.ColorJitter(brightness=brightness, contrast=contrast), # Change légèrement la luminosité
+        transforms.RandomRotation(degrees=degrees),       # Rotation légère (en mettant du noir 0 sur le côte)
+        transforms.RandomVerticalFlip(p=0.5),        # Les mammo peuvent être inversées
+        transforms.ToTensor(),
+        transforms.Lambda(fonction), 
+        # --- AJOUTS POUR LUTTER CONTRE L'OVERFITTING ---
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    transform = transforms.Compose([
+        transforms.Resize((resolution, resolution)),
+        transforms.ToTensor(),
+        transforms.Lambda(fonction), 
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+    train_dataset_full = ImageFolder(root=data_path, transform=train_transform)
+    val_dataset_full = ImageFolder(root=data_path, transform=transform)
+
+    label = np.array(val_dataset_full.targets)
+    print(f"Nombre total d'images : {len(label)}")
+    print(f"Classes détectées : {val_dataset_full.classes}")
+    print(f"Distribution des classes : {Counter(label)}")
+    print(f"accuracy random val : {1-(label.sum()/len(label))}")
+
+    #on équilibre la proportion de labels 0 et 1 dans le dataset d'entraînement
+    sss1 = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=random_number)
+    for train_val_index, test_index in sss1.split(np.zeros(len(label)), label):
+        train_val_indices = train_val_index
+        test_indices = test_index
+
+    train_val_labels = label[train_val_indices]
+    new_val_size = val_size / (train_size + val_size)
+    sss2 = StratifiedShuffleSplit(n_splits=1, test_size=new_val_size, random_state=random_number)
+    for train_index, val_index in sss2.split(np.zeros(len(train_val_labels)), train_val_labels):
+        train_indices = train_val_indices[train_index]
+        val_indices = train_val_indices[val_index]
+
+    print(f"Train set: {len(train_indices)}")
+    print(f"Validation set: {len(val_indices)}")
+    print(f"Test set: {len(test_indices)}")
+
+    # Weighted Sampler pour équilibrer les classes
+    train_labels = label[train_indices]
+    class_counts = Counter(train_labels)
+    print(f"Distribution dans train : {class_counts}")
+
+    num_labels = len(train_labels)
+    weight_for_class = {cl: num_labels / count for cl, count in class_counts.items()}
+    sample_weights = [weight_for_class[l] for l in train_labels]
+    sample_weights_tensor = torch.DoubleTensor(sample_weights)
+
+    print(f"Poids par classe : {weight_for_class}")
+
+    sampler = WeightedRandomSampler(
+        weights=sample_weights_tensor, 
+        num_samples=len(sample_weights_tensor), 
+        replacement=True
+    )
+
+    train_set = Subset(train_dataset_full, train_indices)
+    val_set = Subset(val_dataset_full, val_indices)
+    test_set = Subset(val_dataset_full, test_indices)
+
+    train_loader = DataLoader(train_set, batch_size=batch_size, num_workers=4, pin_memory=True, sampler=sampler)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
+    return(train_loader,val_loader,test_loader)
 
 
-def train_and_validate(
-    model, 
-    train_loader, 
-    val_loader, 
-    criterion, 
-    optimizer,
+def configurate_model(
     device,
-    num_epochs,
     num_class,
-    scheduler
+    dropout,
+    factor,
+    weight_decay,
+    patience,
+    threshold
 ):
-    model.to(device)
-    global best_acc
-    patience_limite=5
-    patience_compteur=0
-    for epoch in range(num_epochs):
-        print(patience_compteur)
-        print(f"\n--- Époque {epoch+1}/{num_epochs} ---")
-
-        train_loss=train_model(model, train_loader, val_loader, criterion, optimizer)
-        print(f"Loss d'entraînement: {train_loss:.4f}")
-
-        val_loss, val_acc=validate_model(model, val_loader, criterion, device)
-        print(f"Loss de validation: {val_loss:.4f} | Accuracy de validation: {val_acc:.4f}")
-
-        scheduler.step(val_acc)
-
-        current_lr=optimizer.param_groups[0]['lr']
-        print(f"Taux d'apprentissage actuel: {current_lr}")
-
-        if best_acc < val_acc:
-            best_acc=val_acc
-            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-            torch.save(model.state_dict(), f"best_resnet18_model_ordonnanceur_{timestamp}.pth")
-            patience_compteur=0
-            print("Modèle sauvegardé.")
-        else:
-            patience_compteur+=1
-            if patience_compteur >= patience_limite:
-                print("Arrêt anticipé déclenché.")
-                break
-    print("\nEntraînement terminé.")
-
-    
+    model = models.resnet18(weights=ResNet18_Weights.DEFAULT)
+    model.fc = nn.Sequential(
+        nn.Dropout(p=dropout), # Désactive 50% des neurones aléatoirement à chaque itération
+        nn.Linear(model.fc.in_features, num_class)
+    )
+    device = torch.device(device)
+    model = model.to(device)
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=factor, patience=patience, threshold=threshold)
+    return model, criterion, optimizer, scheduler
